07fd476dfc54b1cda2675ff21e1f25a2ce2f03a9
Jared Carey <itspandorux@gmail.com>
2017-08-28 17:38:52 +1000

    Refined Task 2
    - Renamed CopyObj.cs to gameController.cs
    - Changed lists within gameController.cs to arrays
    - GameObjects are now dynamically created

---
+++ Assets/Scripts/gameController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameController : MonoBehaviour
+{
+
+    public int numberOfGameObjects = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Create the game object
+        GameObject obj1 = new GameObject();
+        GameObject obj2 = new GameObject();
+
+        // Container
+        GameObject[] gameObjArray = new GameObject[numberOfGameObjects];
+
+        // Add the components
+        for (int i = 0; i < gameObjArray.Length; i++)
+        {
+            gameObjArray[i] = new GameObject();
+
+            // Add all the scripts
+            gameObjArray[i].AddComponent<graphicalObject>();
+            gameObjArray[i].AddComponent<IGB283Transform>();
+
+            // Set the size scale and rotational speed
+            IGB283Transform tran = gameObjArray[i].GetComponent<IGB283Transform>();
+
+            tran.rotSpeed = (i + 1) * 20;
+            tran.sizeScaleSpeed = (float)(i + 1) / 4;
+
+            // Se graphical size
+            graphicalObject go = gameObjArray[i].GetComponent<graphicalObject>();
+
+            go.xSize = 5;
+            go.ySize = 5;
+
+            // Finally move them sideways
+            Vector3 pos = gameObjArray[i].transform.position;
+            pos.x = -5 + 10 * i;
+            gameObjArray[i].transform.position = pos;
+        }
+
+    }
+}
