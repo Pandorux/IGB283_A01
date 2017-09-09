459982aef410c2fd7d729e2d72f826ba582f5bf4
Jared Carey <itspandorux@gmail.com>
2017-09-09 21:20:32 +1000

    Transferred Transform Relevant Functionality to IGB283Transform
    - Only Functionality from GraphicalObject was Transferred to IGB283Transform

--- Assets/Scripts/IGB283Transform.cs
+++ Assets/Scripts/IGB283Transform.cs
@@ -2,9 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class IGB283Transform {
+[RequireComponent(typeof(MeshFilter))]
+public class IGB283Transform : MonoBehaviour {
 
-    public static Matrix3x3 TransMatrix(Vector3 offset)
+    public float rotSpeed = 1;
+    public float xSpeed = 1, ySpeed = 1;
+    public float xScaSpeed = 0.25f, yScaSpeed = 0.25f;
+    public Quaternion initialScale = new Quaternion(1, 1, 1, 1);
+    public Vector3 initialPosition = new Vector3(0, 0, 0);
+
+    private Mesh mesh;
+
+    void Start()
+    {
+        mesh = this.GetComponent<MeshFilter>().mesh;
+
+        Matrix3x3 t = TransMatrix(initialPosition);
+        Matrix3x3 s = ScaleMatrix(initialScale);
+        Matrix3x3 m = t * s;
+        Vector3[] verts = mesh.vertices;
+
+        mesh.vertices = TranslateMesh(m, verts);
+        mesh.RecalculateBounds();
+        this.GetComponent<MeshRenderer>().material.shader = Shader.Find("Unlit/Color");
+    }
+
+    void Update()
+    {
+        Vector3 rotOrigin = this.GetComponent<MeshRenderer>().bounds.center;
+        Matrix3x3 t = TransMatrix(new Vector3(xSpeed * Time.deltaTime, ySpeed * Time.deltaTime, 1));
+        Matrix3x3 m = CalculateRotatation(rotOrigin, rotSpeed);
+        m = m * t;
+
+        mesh.vertices = TranslateMesh(m, mesh.vertices);
+        mesh.RecalculateBounds();
+    }
+
+    public Matrix3x3 TransMatrix(Vector3 offset)
     {
         Matrix3x3 m = new Matrix3x3();
 
@@ -15,7 +49,7 @@ public static class IGB283Transform {
         return m;
     }
 
-    public static Matrix3x3 ScaleMatrix(Quaternion scale)
+    public Matrix3x3 ScaleMatrix(Quaternion scale)
     {
         Matrix3x3 m = new Matrix3x3();
 
@@ -26,7 +60,7 @@ public static class IGB283Transform {
         return m;
     }
 
-    public static Matrix3x3 RotMatrix(float angle)
+    public Matrix3x3 RotMatrix(float angle)
     {
 
         Matrix3x3 m = new Matrix3x3();
@@ -36,5 +70,49 @@ public static class IGB283Transform {
         m.SetRow(2, new Vector3(0.0f, 0.0f, 1.0f));
 
         return m;
+    }
+
+    Matrix3x3 CalculateRotatation(Vector3 originRot, float angle)
+    {
+        Matrix3x3 originNeg = TransMatrix(-originRot);
+        Matrix3x3 origin = TransMatrix(originRot);
+        Matrix3x3 r = RotMatrix(rotSpeed * Time.deltaTime);
+        Matrix3x3 m = origin * r * originNeg;
+        return m;
+    }
+
+    Vector3[] TranslateMesh(Matrix3x3 trans, Vector3[] verts)
+    {
+
+        for (int i = 0; i < verts.Length; i++)
+        {
+            Vector3 v = trans.MultiplyPoint(verts[i]);
+            ChangeDirection(v.x, v.y);
+            verts[i].x = v.x;
+            verts[i].y = v.y;
+        }
+
+        return verts;
+    }
+
+    void ChangeDirection(float xLoc, float yLoc)
+    {
+        if (xLoc >= 1)
+        {
+            xSpeed = -Mathf.Abs(xSpeed);
+        }
+        else if (xLoc <= -1)
+        {
+            xSpeed = Mathf.Abs(xSpeed);
+        }
+
+        if (yLoc >= 1)
+        {
+            ySpeed = -Mathf.Abs(ySpeed);
+        }
+        else if (yLoc <= -1)
+        {
+            ySpeed = Mathf.Abs(ySpeed);
+        }
     }
 }
--- Assets/Scripts/graphicalObject.cs
+++ Assets/Scripts/graphicalObject.cs
@@ -5,16 +5,11 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter))]
 [RequireComponent(typeof(MeshRenderer))]
 [RequireComponent(typeof(ColourLerp))]
+[RequireComponent(typeof(IGB283Transform))]
 public class GraphicalObject : MonoBehaviour {
 
-    public float rotSpeed = 1;
-    public float xSpeed = 1, ySpeed = 1;
     public int xSize = 5, ySize = 5;
-    public float xScalar = 0.25f, yScalar = 0.25f;
     public Color32 colour00 = new Color32(0, 0, 0, 255), colour01 = new Color32(255, 255, 255, 255);
-
-    public Quaternion initialScale = new Quaternion (1, 1, 1, 1);
-    public Vector3 initialPosition = new Vector3(0, 0, 0);
 
     private Mesh mesh;
     private Vector3 rotOrigin;
@@ -34,35 +29,11 @@ public class GraphicalObject : MonoBehaviour {
 
         // References the elements of vertices and colors to create a face
         mesh.triangles = ConstructTriangles(mesh.vertices.Length);
-
-        Matrix3x3 T = IGB283Transform.TransMatrix(initialPosition);
-        Matrix3x3 S = IGB283Transform.ScaleMatrix(initialScale);
-        Matrix3x3 M = T * S;
-        Vector3[] verts = mesh.vertices;
-
-        for(int i = 0; i < verts.Length; i++)
-        {
-            Vector3 v = M.MultiplyPoint(verts[i]);
-
-            verts[i].x = v.x;
-            verts[i].y = v.y;
-        }
-
-        mesh.vertices = verts;
-        mesh.RecalculateBounds();
-        this.GetComponent<MeshRenderer>().material.shader = Shader.Find("Unlit/Color");
     }
 
     void Update()
     {
-        rotOrigin = this.GetComponent<MeshRenderer>().bounds.center;
-        Matrix3x3 t = IGB283Transform.TransMatrix(new Vector3 (xSpeed * Time.deltaTime, ySpeed * Time.deltaTime, 1));
-        Matrix3x3 m = Rotate(rotOrigin, rotSpeed);
-        m = m * t;
 
-        mesh.vertices = TranslateMesh(m, mesh.vertices);
-        mesh.RecalculateBounds();
-        UpdateColour(rotOrigin);
     }
 
     Vector3[] ConstructVecticeArray()
@@ -117,50 +88,6 @@ public class GraphicalObject : MonoBehaviour {
         }
 
         return test.ToArray();
-    }
-
-    Matrix3x3 Rotate(Vector3 originRot, float angle)
-    {
-        Matrix3x3 originNeg = IGB283Transform.TransMatrix(-rotOrigin);
-        Matrix3x3 origin = IGB283Transform.TransMatrix(rotOrigin);
-        Matrix3x3 r = IGB283Transform.RotMatrix(rotSpeed * Time.deltaTime);
-        Matrix3x3 m = origin * r * originNeg;
-        return m;
-    }
-
-    Vector3[] TranslateMesh(Matrix3x3 trans, Vector3[] verts)
-    {
-
-        for (int i = 0; i < verts.Length; i++)
-        {
-            Vector3 v = trans.MultiplyPoint(verts[i]);
-            ChangeDirection(v.x, v.y);
-            verts[i].x = v.x;
-            verts[i].y = v.y;
-        }
-
-        return verts;
-    }
-
-    void ChangeDirection(float xLoc, float yLoc)
-    {
-        if (xLoc >= 1)
-        {
-            xSpeed = -Mathf.Abs(xSpeed);
-        }
-        else if (xLoc <= -1)
-        {
-            xSpeed = Mathf.Abs(xSpeed);
-        }
-
-        if (yLoc >= 1)
-        {
-            ySpeed = -Mathf.Abs(ySpeed);
-        }
-        else if (yLoc <= -1)
-        {
-            ySpeed = Mathf.Abs(ySpeed);
-        }
     }
 
     void UpdateColour(Vector3 meshCentre)
