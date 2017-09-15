1d23f9985a5cec6c4086c79c8292adf618ad2fa3
Jared Carey <itspandorux@gmail.com>
2017-09-15 20:46:52 +1000

    Completed Documentation

--- Assets/Scripts/ColourLerp.cs
+++ Assets/Scripts/ColourLerp.cs
@@ -7,21 +7,20 @@ public class ColourLerp : MonoBehaviour {
 
     public Color32 colour00 = new Color32(0, 0, 0, 0), colour01 = new Color32(255, 255, 255, 255);
 
-
 	void Start () {
+        // Sets the shader to unlit to make the colour change easier to see
         this.GetComponent<MeshRenderer>().material.shader = Shader.Find("Unlit/Color");
     }
-	
 
 	void Update () {
         Vector3 meshOrigin = this.GetComponent<MeshRenderer>().bounds.center;
         UpdateColour(meshOrigin);
 	}
 
+    // Uses the mesh location to step between the two colours
     public Color32 ColourStep(Vector3 meshCentre)
     {
         float step = Mathf.Clamp((meshCentre.x + 1) / 2, 0, 1);
-        Debug.Log(step);
         return Color32.Lerp(colour00, colour01, step);
     }
 
